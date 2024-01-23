b70c5ce9e0d3a3fe30acf802feb0ca7bf6d83677
Gomathi-1999 <154083626+Gomathi-1999@users.noreply.github.com>
2024-01-23 23:18:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const colorDisplay = document.getElementById('color-display');
-    const flipButton = document.getElementById('flip-button');
-  
-    // Function to generate a random hex color
-    function getRandomColor() {
-      return '#' + Math.floor(Math.random()*16777215).toString(16);
-    }
-  
-    // Function to update the color display
-    function updateColor() {
-      const randomColor = getRandomColor();
-      document.body.style.backgroundColor = randomColor;
-      colorDisplay.textContent = randomColor;
-    }
-  
-    // Event listener for the flip button
-    flipButton.addEventListener('click', updateColor);
-  
-    // Initial color update
-    updateColor();
-  });
+const days=document.querySelector("#days");
+const hours=document.querySelector("#hours");
+const minutes=document.querySelector("#minutes");
+const seconds=document.querySelector("#seconds");
+
+seconds.innerHTML="Joes"
+
+function UpdateTime(){
+const currentYear=new Date().getFullYear();
+const newYear=new Date(`January 1 ${currentYear+1} 00:00:00`);
+const currentDate=new Date();
+const diff=newYear-currentDate;
+const d=Math.floor(diff/1000/60/60/24);
+const h=Math.floor((diff/1000/60/60)%24);
+const m=Math.floor((diff/1000/60)%60);
+const s=Math.floor((diff/1000)%60);
+
+days.innerHTML=d<10?"0"+d:d;
+hours.innerHTML=h<10?"0"+h:h;
+minutes.innerHTML=m<10?"0"+m:m;
+seconds.innerHTML=s<10?"0"+s:s;
+}
+
+setInterval(UpdateTime,1000);
+
